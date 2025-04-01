2ec4fc101a4e636657747536a32bf2bab534ba5e
Hyrum Whetten <jeromehyhy@gmail.com>
2025-04-01 14:43:11 +0000

    My first Variables

---
+++ age.c
@@ -0,0 +1,16 @@
+/*This program takes age input from the user
+It stores it in the age variable
+And, prints the value using printf()*/
+
+#include <stdio.h>
+
+int main() {
+    //Create integer variable
+    int age = 17;
+
+    //Print the age variable
+    printf("Age: %d\n", age);
+    return 0;
+
+}
+
