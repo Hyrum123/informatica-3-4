8e1107f207223ade4635a92c5f79c0208e09b7ff
Hyrum Whetten <jeromehyhy@gmail.com>
2025-04-09 14:40:55 +0000

    Now I used scanf

--- variables-operations.c
+++ variables-operations.c
@@ -2,24 +2,39 @@
 
 int main() {
     //rectangle
-    int height = 50;
-    int length = 100;
+
+    int height;
+    int length;
+    printf("Enter a rectangle height: ");
+    scanf("%d", &height);
+    printf("Enter a rectangle length: ");
+    scanf("%d", &length);
     int area = height*length;
     int perimeter = height+height+length+length;
     printf("Area: %d\n", area);
-    printf("Perimeter: %d\n", perimeter);
+    printf("Perimeter: %d\n\n", perimeter);
 
     //temperature
-    int fahrenheit = 100;
+    int fahrenheit;
+    printf("Enter a degree in fahrenheit: ");
+    scanf("%d", &fahrenheit);
     int celsius = (fahrenheit-32)/1.8;
-    printf("\n%dºF = ", fahrenheit);
-    printf("%dºC\n", celsius);
+    printf("%dºF = ", fahrenheit);
+    printf("%dºC\n\n", celsius);
 
     //minutes and hours
-    int mins = 349;
+    int mins;
+    printf("Enter a number of minutes: ");
+    scanf("%d", &mins);
     int hours = mins/60;
     int extramins = mins%60;
-    printf("\n%d minutes is equal to ", mins);
+    printf("%d minutes is equal to ", mins);
     printf("%d hours and ", hours);
-    printf("%d minutes.\n", extramins);
+    printf("%d minutes.\n\n", extramins);
+
+    int radius;
+    printf("Enter a radius of a circle: ");
+    scanf("%d", &radius);
+    int circleArea = 3.14*(radius^2);
+    printf("The are of the circle is: %d\n\n", circleArea);
 }
