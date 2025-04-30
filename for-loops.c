cbfc78bc08dd62fb76786fb2a6f778113519f9e8
Hyrum Whetten <jeromehyhy@gmail.com>
2025-04-30 14:42:42 +0000

    for-loops

--- for-loops.c
+++ for-loops.c
@@ -9,5 +9,14 @@ int main() {
         bigSum = bigSum + sum;
     }
     printf("%d\n", bigSum);
+    int num2;
+    printf("Type any natural number: ");
+    scanf("%d", &num2);
+    int bigMul = 1;
+    for(int mul = num2; mul > 0; mul--) {
+        bigMul = mul * bigMul;
+    }
+    printf("%d\n", bigMul);
+
     return(0);
 }
