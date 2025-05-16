c9fad97730f76c8b36cf8923e68db5654c209a4e
Hyrum Whetten <jeromehyhy@gmail.com>
2025-05-16 14:50:43 +0000

    so far

---
+++ functions.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int planetAge(float a, float p);
+
+int main() {
+    const float mercury = 0.2408467;
+    const float venus = 0.61519726;
+    const float earth = 1;
+    const float mars = 1.8808158;
+    const float jupiter = 11.862615;
+    const float saturn = 29.447498;
+    const float uranus = 84.016846;
+    const float neptune = 164.79132;
+
+    int age, planet;
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+    printf("Select a planet to see your age if you lived on that planet\n1. Mercury\n2. Venus\n3. Earth\n4. Mars\n5. Jupiter\n6. Saturn\n7. Uranus\n8. Neptune\nEnter an option: ");
+    scanf("%d", &planet);
+    float printResult;
+
+    if(planet == 1) {
+        printResult = planetAge(age, mercury);
+    } else if(planet == 2) {
+        printResult = planetAge(age, venus);
+    }
+    printf("%f\n", printResult);
+}
+
+int planetAge(float a, float p) {
+    float result = a * p;
+    return result;
+}
